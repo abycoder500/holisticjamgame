d973e8fbf0ada5c42409927aa5a32af703763f0c
heckert-gamedev <unity.github@nostromo.in-berlin.de>
2022-07-15 18:19:58 +0200

    Allow broadcasted events to be fired with a delay on the target object

--- jamgame/Assets/HolisticJam/Scripts/InteractionBroadcasterTarget.cs
+++ jamgame/Assets/HolisticJam/Scripts/InteractionBroadcasterTarget.cs
@@ -6,12 +6,18 @@ namespace HolisticJam
 {
     public class InteractionBroadcasterTarget : InteractionTarget
     {
+        [SerializeField] float _broadcastDelay = 0f;
+
         override public bool ActivateTargetAction()
         {
-            SendMessageOptions options = SendMessageOptions.DontRequireReceiver;
-
-            BroadcastMessage(nameof(OnInteractionBroadcast), options);
+            Invoke(nameof(PerformBroadcast), _broadcastDelay);
             return true;
+        }
+
+        private void PerformBroadcast()
+        {
+            SendMessageOptions options = SendMessageOptions.DontRequireReceiver;
+            BroadcastMessage(nameof(OnInteractionBroadcast), options);
         }
 
         void OnInteractionBroadcast()
